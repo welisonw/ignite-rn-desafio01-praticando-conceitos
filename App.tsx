b3a0b53c58f6945c825ca88bba456ec9df5c80f9
Welison Wessling <wwelisonw@gmail.com>
2023-11-24 14:30:27 -0300

    feat: added components in App

--- App.tsx
+++ App.tsx
@@ -1,14 +1,30 @@
 import { StatusBar } from "expo-status-bar";
-import { View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import Header from "./src/components/Header";
 import AddTask from "./src/components/AddTask";
+import { ListTasks } from "./src/components/ListTasks";
+import { useState } from "react";
+import { Task } from "./src/interfaces/Task";
 
 export default function App() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
   return (
-    <View>
+    <View style={styles.container}>
       <StatusBar style="inverted" backgroundColor="transparent" translucent />
       <Header />
-      <AddTask />
+      <AddTask
+        tasks={tasks}
+        setTasks={setTasks}
+      />
+      <ListTasks tasks={tasks} setTasks={setTasks} />
     </View>
   );
-};
+}
+
+const styles = StyleSheet.create({
+  container: {
+    height: "100%",
+    backgroundColor: "#1A1A1A",
+  },
+});
