2b89e16b935a68abb340e06e4029a03fe1c6509b
Welison Wessling <wwelisonw@gmail.com>
2023-11-24 15:40:04 -0300

    feat: created tokens

---
+++ src/tokens/src/colors.ts
@@ -0,0 +1,19 @@
+
+export const colors = {
+  base: {
+    gray100: "#F2F2F2",
+		gray200: "#D9D9D9",
+		gray300: "#808080",
+		gray400: "#333333",
+		gray500: "#262626",
+		gray600: "#1A1A1A",
+		gray700: "#0D0D0D",
+  },
+
+  brand: {
+		blue: "#4EA8DE",
+		blue_dark: "#1E6F9F",
+    purple: "#8284FA",
+		purple_dark: "#5E60CE",
+  },
+};
---
+++ src/tokens/src/fonts.ts
@@ -0,0 +1,13 @@
+
+export const fonts = {
+  fontFamilies: {
+		regular: 'Inter_400Regular',
+		bold: 'Inter_700Bold',
+	},
+  
+	fontSizes: {
+		sm: 12,
+    md: 14,
+		lg: 16,
+	},
+};
